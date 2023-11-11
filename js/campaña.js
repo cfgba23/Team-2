3ce4056d4fd2086be56c2d4b7323b46d2f752226
Mailen Lorena Alvarez <m.labarrere2@gmail.com>
2023-11-11 19:16:08 -0300

    agregar cambios campana

---
+++ js/campaña.js
@@ -0,0 +1,18 @@
+document.getElementById('miBoton').addEventListener('click', function() {
+    console.log("hola");
+    Swal.fire({
+      title: '¡Atención!',
+      text: 'Debes loguearte para crear una nueva campaña',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Quiero Loguearme'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Redirige al usuario a la URL deseada
+        window.location.href = '/login';
+      }
+    });
+  });
